24f0942e5a08c0822faaaeb320777fc855ab94f8
Lee Seung Yun <ileilliat@gmail.com>
2023-09-13 11:54:23 +0900

    초기화, 고정 모드 플래그와 테마 확인용 다이얼로그 추가

--- app/src/main/java/com/nexon/darkwithlegacy/MainActivity.kt
+++ app/src/main/java/com/nexon/darkwithlegacy/MainActivity.kt
@@ -2,17 +2,24 @@ package com.nexon.darkwithlegacy
 
 import android.app.ActionBar
 import android.app.Activity
+import android.app.UiModeManager
 import android.content.Context
 import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.util.TypedValue
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toolbar
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.edit
 
 class MainActivity : Activity() {
     private var _appCompatDelegate: AppCompatDelegate? = null
@@ -21,8 +28,21 @@ class MainActivity : Activity() {
     // AppCompat 분기 사용 여부
     // Activity -> true, AppCompatActivity -> false 로 지정
     private val appCompatFlag = true
+    private val clearFlag = false
+    private val fixedModeFlag = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (clearFlag) {
+            clearMode()
+            finish()
+            return
+        }
+
+        if (fixedModeFlag) {
+            fixedMode(Mode.DARK)
+        }
+
+        Log.d("LSYD", "onCreate: ${AppCompatDelegate.getDefaultNightMode()}")
         if (appCompatFlag) {
             _appCompatDelegate = AppCompatDelegate.create(this, null).apply {
                 // this: AppCompatDelegate
@@ -35,6 +55,14 @@ class MainActivity : Activity() {
                 }
                 findViewById<TextView>(R.id.resource_text)?.text =
                     resources.getString(R.string.mode)
+                findViewById<Button>(R.id.open_alert_button)?.setOnClickListener {
+                    AlertDialog.Builder(this@MainActivity)
+                        .setTitle(R.string.app_name)
+                        .setMessage(R.string.mode)
+                        .setPositiveButton(R.string.action_ok, null)
+                        .create()
+                        .show()
+                }
             }
         } else {
             super.onCreate(savedInstanceState)
@@ -43,6 +71,84 @@ class MainActivity : Activity() {
                 ThemeChangeDialogFragment().show(fragmentManager, "dialog")
             }
             findViewById<TextView>(R.id.resource_text).text = resources.getString(R.string.mode)
+            findViewById<Button>(R.id.open_alert_button)?.setOnClickListener {
+                AlertDialog.Builder(this@MainActivity)
+                    .setTitle(R.string.app_name)
+                    .setMessage(R.string.mode)
+                    .setPositiveButton(R.string.action_ok, null)
+                    .create()
+                    .show()
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val typedValue = TypedValue()
+            theme.resolveAttribute(android.R.attr.isLightTheme, typedValue, true)
+            Log.d(
+                "LSYD", "onCreate: ${
+                    typedValue.data != 0
+                }"
+            )
+        }
+    }
+
+    enum class Mode {
+        DARK, LIGHT, AUTO
+    }
+
+    private fun clearMode() {
+        setMode(Mode.AUTO)
+        Log.e("LSYD", "CLEAR MODE COMPLETE")
+    }
+
+    private fun fixedMode(mode: Mode) {
+        setMode(mode)
+        Log.e("LSYD", "FIX MODE - $mode COMPLETE")
+    }
+
+    private fun setMode(mode: Mode) {
+        data class ModeComponents(
+            val isCustom: Boolean,
+            val isDark: Boolean,
+            val uiMode: Int,
+            val appCompatMode: Int,
+        )
+
+        val (isCustom, isDark, uiMode, appCompatMode) = when (mode) {
+            Mode.DARK -> ModeComponents(
+                isCustom = true,
+                isDark = true,
+                uiMode = UiModeManager.MODE_NIGHT_YES,
+                appCompatMode = AppCompatDelegate.MODE_NIGHT_YES
+            )
+            Mode.LIGHT -> ModeComponents(
+                isCustom = true,
+                isDark = false,
+                uiMode = UiModeManager.MODE_NIGHT_NO,
+                appCompatMode = AppCompatDelegate.MODE_NIGHT_NO
+            )
+            Mode.AUTO -> ModeComponents(
+                isCustom = false,
+                isDark = false,
+                uiMode = UiModeManager.MODE_NIGHT_AUTO,
+                appCompatMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            )
+        }
+        getSharedPreferences(ThemeChangeDialogFragment.KEY, Context.MODE_PRIVATE)?.apply {
+            edit {
+                putBoolean(ThemeChangeDialogFragment.SETTINGS_DARK, isDark)
+                putBoolean(ThemeChangeDialogFragment.SETTINGS_CUSTOM, isCustom)
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            (getSystemService(Context.UI_MODE_SERVICE) as? UiModeManager)?.apply {
+                setApplicationNightMode(
+                    uiMode
+                )
+            }
+        } else {
+            AppCompatDelegate.setDefaultNightMode(
+                appCompatMode
+            )
         }
     }
 
@@ -52,8 +158,10 @@ class MainActivity : Activity() {
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
+        Log.d("LSYD", "onConfigurationChanged start")
         super.onConfigurationChanged(newConfig)
         _appCompatDelegate?.onConfigurationChanged(newConfig)
+        Log.d("LSYD", "onConfigurationChanged end")
     }
 
     override fun onStart() {
